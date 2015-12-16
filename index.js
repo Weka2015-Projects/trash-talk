98e07f49af4db1c0a5ee389e9f42c333cf6fcc50
Aaron Robb <email": null>
2015-12-17 10:13:35 +1300

    basic package

---
+++ index.js
@@ -0,0 +1,14 @@
+// making the server
+// research websocket and play with websocket 
+// 
+
+// make the client
+
+
+// making the game   
+// 1. what a function that compare the move of 2 player
+// 2. a function that declare a winner 
+// 3. compare the move if rock i
+
+
+// test the game
