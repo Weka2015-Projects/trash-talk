1d3ce68063c296b0803ca89618825628db8fa251
Aaron Robb <email": null>
2015-12-17 14:50:24 +1300

    got input working

--- client.js
+++ client.js
@@ -1,8 +1,40 @@
 'use strict'
 const io = require('socket.io-client')
 const socket = io.connect('http://localhost:3000')
+const R = require('ramda')
+const inquirer = require('inquirer')
+
+
+const myLastMessages = []
+
 socket.on('connect', () => {
-  socket.on('message', (data) => {
-    console.log('we got a message: ', data)
+  inquirer.prompt([introQuestion], (answers) => {
+    socket.emit('message', answers)
+    myLastMessages.push(answers)
   })
+  socket.on('message', (data) => {
+    if (data !== R.last(myLastMessages)){
+    console.log('we got a message: ', data)
+  }})
 })
+
+const introQuestion = {
+  validate: function(input) {
+    
+// Declare function as asynchronous, and save the done callback 
+  done = this.async()
+    
+  // Do async stuff 
+  setTimeout(function() {
+    if (typeof input !== "string") {
+    // Pass the return value in the done callback 
+      done("You need to provide a number")
+      return;
+    }
+      // Pass the return value in the done callback 
+    done(true)
+    }, 3000)
+  },
+  message: 'Whats your user name',
+  name: 'username'
+}
--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 'use strict'
 const R = require('ramda')
 const color = require('colors')
+const emoji = require('emoji-and-emoticons')
 
 const server = require('http').createServer()
 const io = require('socket.io')(server)
+  
 
+const chatLog = []
+const sockets = []
 
 io.on('connection', (socket) => {
+  sockets.push(socket)
   const userAddress = R.replace(/\:\:[a-z]+\:/g, '', socket.handshake.address)
+  console.log('connection from '.green + userAddress.yellow)
   
-  console.log('connection from ' + userAddress)
   socket.on('message', (data) => {
-    socket.emit('message', data)
-  })
-
-  socket.on('event', (data) => {
+    broadcast('message', data);
   })
   socket.on('disconnect', () => {
-    console.log(userAddress + ' disconnected')
+    console.log(userAddress.yellow + ' disconnected'.red)
   })
 })
+
+const broadcast = (event, data) => {
+  sockets.forEach((socket) => {
+    socket.emit(event, data)
+  })
+}
+
 server.listen(3000)
 
