44a4b39089dbcb3112a9e652f8037cf2c65fea48
Aaron Robb <email": null>
2015-12-17 15:01:12 +1300

    inquirer

--- client.js
+++ client.js
@@ -14,27 +14,53 @@ socket.on('connect', () => {
   })
   socket.on('message', (data) => {
     if (data !== R.last(myLastMessages)){
-    console.log('we got a message: ', data)
+    console.log(data)
+    inquirer.prompt([newQuestion(data)], (answers) => {
+      socket.emit('message', answers)
+      myLastMessages.push(answers)
+  })
   }})
 })
 
 const introQuestion = {
-  validate: function(input) {
-    
-// Declare function as asynchronous, and save the done callback 
-  done = this.async()
-    
-  // Do async stuff 
-  setTimeout(function() {
-    if (typeof input !== "string") {
-    // Pass the return value in the done callback 
-      done("You need to provide a number")
-      return;
-    }
+ validate: function(input) {
+ 
+    // Declare function as asynchronous, and save the done callback 
+    var done = this.async();
+ 
+    // Do async stuff 
+    setTimeout(function() {
+      if (typeof input !== "string") {
+        // Pass the return value in the done callback 
+        done("You need to provide a number");
+        return;
+      }
       // Pass the return value in the done callback 
-    done(true)
+      done(true);
     }, 3000)
   },
-  message: 'Whats your user name',
+  message: 'Whats your user name \n',
   name: 'username'
 }
+
+const newQuestion = (message) => {
+  return {
+  validate: function(input) {
+ 
+    // Declare function as asynchronous, and save the done callback 
+    var done = this.async();
+ 
+    // Do async stuff 
+    setTimeout(function() {
+      if (typeof input !== "string") {
+        // Pass the return value in the done callback 
+        done("You need to provide a number");
+        return;
+      }
+      // Pass the return value in the done callback 
+      done(true);
+    }, 3000)
+  },
+  message: message.username + '\n',
+  name: 'chat'
+}}
